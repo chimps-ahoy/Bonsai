c7542b81d81dd50d4493cab97d4c3fda59ca58e9
sebastien <sebastien.peterson.boudreau@gmail.com>
2024-03-17 07:08:38 -0300

    updated structure to use weak reference and refcell. MONSTROUS single expression from() method

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,37 @@
 mod tiles;
-use crate::tiles::Tiling;
+use crate::tiles::{Region,RegionKind};
+use crate::tiles::Side as S;
+use crate::tiles::Orientation as O;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 fn main() {
+    let a = Rc::new(RefCell::new(Region{
+        kind: RegionKind::Client{
+            window: 0
+        },
+        container: None,
+        tags: 0,
+    }));
+    let b = Rc::new(RefCell::new(Region{
+        kind: RegionKind::Client{
+            window: 0
+        },
+        container: None,
+        tags: 0,
+    }));
+    let r: Rc<RefCell<Region>> = Rc::new(RefCell::new(Region{
+        kind: RegionKind::Split{
+            subregion: [Some(a.clone()),Some(b.clone())],
+            fact: 0.5,
+            o: O::V,
+        },
+        container: None,
+        tags: 0,
+    }));
+    a.borrow_mut().container = Some(Rc::downgrade(&r.clone()));
+    b.borrow_mut().container = Some(Rc::downgrade(&r.clone()));
+    assert_eq!(a.borrow().from(), Some(S::L));
+    println!("{:?}", a.borrow().from());
     println!("Hello, world!");
 }
--- rust/src/tiles.rs
+++ rust/src/tiles.rs
@@ -1,26 +1,29 @@
 use std::rc::{Rc,Weak};
+use std::cell::RefCell;
 use std::ptr::eq;
 
-enum Orientation {
+pub enum Orientation {
     V = 0b00,
     H = 0b10,
 }
 
-enum Side {
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum Side {
     L = 0b00,
     R = 0b01,
 }
 
-enum Direction {
+pub enum Direction {
     North = Orientation::H as isize | Side::L as isize,
     South = Orientation::H as isize | Side::R as isize,
     East = Orientation::V as isize | Side::L as isize,
     West = Orientation::V as isize | Side::R as isize,
 }
 
-enum RegionKind {
+pub enum RegionKind {//SHOULDNT BE PUB
     Split { 
-        subregion: [Option<Rc<Region>>; 2],
+        subregion: [Option<Rc<RefCell<Region>>>; 2],
         fact: f32,
         o: Orientation,
     },
@@ -29,10 +32,10 @@ enum RegionKind {
     },
 }
 
-struct Region {
-    kind: RegionKind,
-    container: Option<Rc<Region>>,
-    tags: u8,
+pub struct Region {//SHOULDNT BE PUB...?
+    pub kind: RegionKind,
+    pub container: Option<Weak<RefCell<Region>>>,
+    pub tags: u8,
 }
 
 pub struct Tiling {
@@ -42,25 +45,26 @@ pub struct Tiling {
 }
 
 impl Region {
-    fn from(&self) -> Option<Side> {
-        if let RegionKind::Split{subregion,..} = &self.container.as_ref()?.kind {
-            match &subregion[0] {
-                None => None,
-                Some(r) => if eq(Rc::<Region>::as_ptr(&r),self) {
-                    Some(Side::L)
-                } else {
-                    None
-                }.or_else(|| { match &subregion[1] {
-                    None => None,
-                    Some(r) => if eq(Rc::<Region>::as_ptr(&r),self) {
-                        Some(Side::R)
-                    } else {
-                        None
-                    }
-                }})
-            }
+
+    fn subregion(&self) -> Option<&[Option<Rc<RefCell<Region>>>; 2]> {
+        if let RegionKind::Split{subregion,..} = &self.kind {
+            Some(subregion)
         } else {
             None
         }
+    }
+
+    pub fn from(&self) -> Option<Side> {
+        self.container
+            .as_ref()? //the root node is the ONLY node st `from() == None`
+            .upgrade()
+            .unwrap()
+            .borrow()
+            .subregion()
+            .unwrap()
+            .iter()
+            .map_while(|sub| sub.as_ref())
+            .zip([Side::L, Side::R])
+            .find_map(|(sub,side)| eq(sub.as_ptr(),self).then_some(side))
     }
 }
