fed6802328a540d4ce97ddaa939fde02586cf322
sebastien <sebastien.peterson.boudreau@gmail.com>
2024-05-25 00:35:48 -0300

    branching

--- lib/config.h
+++
@@ -1,6 +0,0 @@
-static const unsigned int borderpx = 3;
-static const unsigned long border_colour = 0xff0000;
-static const unsigned long bg_colour = 0x0000ff;
-
-static const unsigned int gappx = 6;
-static const unsigned int barpx = 12;
--- lib/util.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTIL
-#define UTIL
-#define MAX(x,y) (((x) > (y)) ? (x) : (y))
-#define MIN(x,y) (((x) < (y)) ? (x) : (y))
-#define NT(x) ((x)^1)
-#endif
