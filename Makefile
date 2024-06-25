375c09f8f2ee182c4f41e4c03159cca5311a649a
sebastien <sebastien.peterson.boudreau@gmail.com>
2024-06-25 18:56:20 -0300

    made Makefile MOSTLY POSIX compliant, but there is no way to handle obj/ with only POSIX behaviour, and i want my obj/ in a directory so i can clean them up easily and stuff idk. might change this some more

--- Makefile
+++ Makefile
@@ -1,13 +1,12 @@
+.POSIX:
 include config.mk
 
 #TODO: these will have to be changed for release to put files in /usr/local/bin/ instead of just running it all from
 #current directory!
 
+DATE != date +"%F@%H:%M:%S"
 run : $(EXEC)
-	$(BINDIR)/$(EXEC) 2>$(LOGDIR)/"$(shell date +"%F@%H:%M:%S").log"
-
-debug : CFLAGS += -g -DDEBUG
-debug : $(EXEC)
+	$(BINDIR)/$(EXEC) 2>$(LOGDIR)/$(DATE).log
 
 $(EXEC) : bin
 	printf "#!/bin/sh\n\nswc-launch $(BINDIR)/$(EXEC)_bin" > $(BINDIR)/$(EXEC)
@@ -15,7 +14,7 @@ $(EXEC) : bin
 
 bin : $(OBJECTS)
 	mkdir -p $(BINDIR)
-	$(CC) $(CFLAGS) $^ -o $(BINDIR)/$(EXEC)_$@
+	$(CC) $(CFLAGS) $(OBJECTS) -o $(BINDIR)/$(EXEC)_$@
 
 $(OBJDIR)%.o : $(SRCDIR)%.c
 	mkdir -p $(OBJDIR)
--- config.mk
+++ config.mk
@@ -1,15 +1,15 @@
 EXEC = Bonsai
 VERSION = 0.0
 
-TOPDIR := $(shell pwd)
-SRCDIR := $(TOPDIR)/src
-BINDIR := $(TOPDIR)/bin
-OBJDIR := $(TOPDIR)/obj
-HDRDIR := $(TOPDIR)/lib
-LOGDIR := $(TOPDIR)/logs
+TOPDIR != pwd
+SRCDIR ::= $(TOPDIR)/src
+BINDIR ::= $(TOPDIR)/bin
+OBJDIR ::= $(TOPDIR)/obj
+HDRDIR ::= $(TOPDIR)/lib
+LOGDIR ::= $(TOPDIR)/logs
 
-SOURCES := $(shell find $(SRCDIR) -name "*.c") 
-OBJECTS := $(patsubst $(SRCDIR)/%.c, $(OBJDIR)/%.o, $(SOURCES))
+SOURCES != find $(SRCDIR) -name "*.c"
+OBJECTS != echo $(SOURCES) | sed -e 's,\.c,.o,g' -e s,$(SRCDIR),$(OBJDIR),g
 
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
@@ -17,4 +17,5 @@ MANPREFIX = $(PREFIX)/share/man
 INCS = -I$(HDRDIR)
 LIBS = -lswc -lwayland-server -lc
 
+CC=/bin/cc
 CFLAGS = -std=c2x -pedantic -Wall $(INCS) $(LIBS) -DVERSION=\"$(VERSION)\" -D_POSIX_C_SOURCE=200112L
